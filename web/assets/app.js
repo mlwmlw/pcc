9fce270fb48bdbe5511d9e13856ac50a56aea17d
mlwmlw <520cutecat@gmail.com>
2014-09-19 00:20:26 +0800

    template engine

---
+++ web/assets/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('myapp', ['ui.bootstrap']);	
+app.config(function($interpolateProvider) {
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
+});
+app.run(function($rootScope, $http, $window) {
+	$http.get('/api/categories').then(function(res) {
+		$rootScope.categories = res.data;
+	});
+	$rootScope.search = function(keyword) {
+		$window.location.href = '/api/keyword/' + keyword;
+	}
+});
+app.filter('money', function() {
+	return function(input) {
+		input = input.replace('.00', '');
+		if(input.slice(-4) == ',000')
+			return input.substr(0, input.length -4) + 'k';
+		else 
+			return input;
+	};
+});
+app.filter('date', function() {
+	return function(input) {
+		return input.replace(/T.+$/, '');	
+	};
+});
+app.filter('option', function () {
+	return function (items, value) {
+		var out = [{}];
+		var reg = new RegExp(value);
+		if(value) {
+			for(var x in items) {
+				if(reg.test(items[x]))
+					out.push(items[x]);
+			}
+			return out;
+		}
+		else if(!value) {
+			return items;
+		}
+	};
+});
