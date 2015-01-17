b226b2c2595a64f2f436e5ace6eb8df7677b39b1
mlwmlw <520cutecat@gmail.com>
2015-01-18 00:10:49 +0800

    item link

--- web/assets/app.js
+++ web/assets/app.js
@@ -43,7 +43,9 @@ app.service('grid', function($filter) {
 					return React.DOM.a({href:"/unit/" + row.unit}, row.unit);
 				}},
 				{ field: "category", displayName: "分類"},
-				{ field: "name", displayName: "標案名稱"},
+				{ field: "name", displayName: "標案名稱", render: function(row) {
+					return React.DOM.a({target: "_blank", href:"//web.pcc.gov.tw/tps/tpam/main/tps/tpam/tpam_tender_detail.do?searchMode=common&scope=F&primaryKey="  + row.key}, row.name);
+				}},
 				{ field: "price", displayName: "金額", render: function(row) {
 					return $filter('money')($filter('currency')(row.price));
 				}},
