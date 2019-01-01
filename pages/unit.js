3f796f7154eb1afd6d64483418520eef6bc9d6df
mlwmlw <520cutecat@gmail.com>
2019-01-02 03:17:29 +0800

    fix warning

--- pages/unit.js
+++ pages/unit.js
@@ -72,15 +72,15 @@ export default class extends React.Component {
     let more = [];
     if(unit.parent.name)
       more = [
-        <span>查詢更多</span>,
-        <span> </span>,
-        <a href={"/unit/" + unit.parent.name}>{unit.parent.name}相關標案</a>, 
-        <span> </span>,
-        <a href={"/units/#!/?parent=" + unit.parent._id}>{unit.parent.name}相關機關</a>,
-        <span> </span>
+        <span key={1}>查詢更多</span>,
+        <span key={2}> </span>,
+        <a key={3} href={"/unit/" + unit.parent.name}>{unit.parent.name}相關標案</a>, 
+        <span key={4}> </span>,
+        <a key={5} href={"/units/#!/?parent=" + unit.parent._id}>{unit.parent.name}相關機關</a>,
+        <span key={6}> </span>
       ]
     if(unit.childs.length > 0)
-      more.push(<a href={"/units/#!/?parent=" + unit._id}>{unit.name}子機關</a>)
+      more.push(<a key={7} href={"/units/#!/?parent=" + unit._id}>{unit.name}子機關</a>)
     return (
       <div className="starter-template">
         <Head>
